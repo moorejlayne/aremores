892d96772803e92a3398e67a1e32126bf6b20c1e
Jacob L Moore <moore.jlayne@gmail.com>
2021-03-17 20:49:13 -0500

    add home anchor to logo

--- src/components/header/aremores-logo.js
+++ src/components/header/aremores-logo.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import logo from '../../assets/aremores-logo.jpeg';
+import { basePath } from '../../paths';
 
 export const AremoresLogo = () => {
 
     return (
         <div className={ 'aremores-header' }>
             <div className={ 'aremores-logo' }>
-                <img alt={ 'AREMORES, LLC. Family Tree Farm & Cabin Rentals'} src={ logo } nopin={'nopin'} />
+               <a href={ basePath }>
+                   <img alt={ 'AREMORES, LLC. Family Tree Farm & Cabin Rentals'} src={ logo } nopin={'nopin'} />
+               </a>
             </div>
         </div>
     )
